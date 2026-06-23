c29301a0401cf74a086025877b09d76d0a7de24f
Christopher-Santas <57292543+Christopher-Santas@users.noreply.github.com>
2026-06-23 12:43:03 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05"; 
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux"; 
+      pkgs = nixpkgs.legacyPackages.${system};
+      
+      runtimeLibs = with pkgs; [];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          typst
+        ];
+
+        buildInputs = runtimeLibs;
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+      };
+    };
+}
