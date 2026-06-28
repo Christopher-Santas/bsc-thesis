bbaa4f217feafd7d778bbb8614dd3e1e7a5dce7d
Christopher-Santas <57292543+Christopher-Santas@users.noreply.github.com>
2026-06-28 15:24:02 +0300

    Create map with color scaling

--- python/main.py
+++ python/main.py
@@ -1,1 +1,35 @@
-print("Hello world")
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("locations.csv")
+
+
+def map(df, color=False):
+    fig = px.scatter_map(
+        df,
+        lat="lat",
+        lon="lon",
+        hover_name="id",
+        color="con" if color else None,
+        color_continuous_scale="Viridis" if color else None,
+        opacity=0.9,
+        size=[20]*len(df),
+        zoom=11.3,
+        height=1000,
+        width=1000,
+        center={
+            'lat': 37.97670606189941, 
+            'lon': 23.726178063333588,
+        },
+        map_style="open-street-map"
+    )
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+
+fig1 = map(df)
+fig1.show()
+
+fig2 = map(df, color=True)
+fig2.show()
